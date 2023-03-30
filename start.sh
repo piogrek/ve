1e81b1ae98a1aefa1c65418c22cf54d65f4eeedb
Piotr Grochowski <piogrek@gmail.com>
2023-03-30 12:27:13 +0100

    start script

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/piogrek/ve/main/worker_linux_amd64 -o worker_linux_amd64
-chmod +x ./worker_linux_amd64
-MQ="amqp://mq:@s.com:5672/"  DB="root:@tcp(s.com:3306)/evs" ./worker_linux_amd64
+cd /root/ve
+git pull
+./worker_linux_amd64
